deffb292195deb92ebff2945b34e9867f9848a31
will <willsmith12289@gmail.com>
2016-06-24 09:48:35 -0400

    got basic portfolio done

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 $('document').ready(function() {
+//execute function when thumb clicked
 	$('.thumbnail').click(function() {
+//"this" thumbnails src
 		var backImg = $(this).attr("src");
-		$('#view').css('background-image', 'url('+ backImg +')');
-		$('#view').hide().fadeIn();
-	$( "button" ).click(function() {
-  		$(this)  = $(this).next();
-	});
-
+//divs background is set to "this" thumbnails src
+		//$('.view').html("<img src='"+backImg+"' id='fillBackground'/>);
+		$('.view').css('background-image', 'url("'+ backImg +'")');
+	if (backImg == "img/beneducePan.jpg") {
+		$('.view').css("background-size", "cover");
+	}else{
+		$('.view').hide().fadeIn(1000);
+		}
 	});
 });
--- portfolio.php
+++ portfolio.php
@@ -10,7 +10,14 @@ include 'header.php';
  		<img src="img/irregularStep.jpg" class="thumbnail">
  		<img src="img/irregblstone.jpg" class="thumbnail">
  		<img src="img/beneduceFire.jpg" class="thumbnail">
-		<img src="" id="view">
+ 		<img src="img/beneducePan.jpg" class="thumbnail">
+ 		<img src="img/regBlueStoneWalk.jpg" class="thumbnail">
+ 		<img src="img/rockwall.jpg" class="thumbnail">
+ 		<img src="img/wallstep.jpg" class="thumbnail">
+ 		<img src="img/wavyGrass.jpg" class="thumbnail">
+ 		<img src="img/maintenanceBut2.jpg" class="thumbnail">
+		<div class="view">
+		</div>
  </section>
  <?php 
 include 'footer.php';
